5327c8ca6e63a48325eb06eef94e2eb251c17a17
Nik Lever <47000827+NikLever@users.noreply.github.com>
2024-04-21 08:38:16 +0100

    Added threejs cookbook

---
+++ courses/threejs-cookbook/complete/physics/pool/Ball.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three';
+import * as CANNON from 'cannon';
+
+class Ball{
+    static RADIUS = 0.05715 / 2;
+    static MASS = 0.17;
+    static MATERIAL = new CANNON.Material("ballMaterial");
+    
+    constructor(game, x, z, id=0) {
+        this.id = id;
+    
+        this.startPosition = new THREE.Vector3(x, Ball.RADIUS, z);
+        
+        this.world = game.world;
+        this.game = game;
+
+        this.rigidBody = this.createBody(x, Ball.RADIUS, z);
+        this.world.addBody(this.rigidBody);
+ 
+        const color = (id==0) ? 0xFFFFFF : 0xFF0000;
+        this.mesh = game.helper.addVisual(this.rigidBody, color);
+
+        this.name = `ball${id}`;
+
+        this.forward = new THREE.Vector3(0,0,-1);
+        this.up = new THREE.Vector3(0,1,0);
+        this.tmpVec = new THREE.Vector3();
+        this.tmpQuat = new THREE.Quaternion();
+    }
+
+    hit(strength=0.6) {
+      this.rigidBody.wakeUp();
+      
+      const theta = this.game.controls.getAzimuthalAngle();
+      this.tmpQuat.setFromAxisAngle(this.up, theta);
+
+      const forward = this.forward.clone().applyQuaternion(this.tmpQuat);
+    
+      const force = new CANNON.Vec3();
+      force.copy(forward);
+      force.scale(strength, force);
+      
+      this.rigidBody.applyImpulse(force, new CANNON.Vec3());
+  }
+    
+  createBody(x,y,z) {
+      const body = new CANNON.Body({
+        mass: Ball.MASS, // kg
+        position: new CANNON.Vec3(x,y,z), // m
+        shape: new CANNON.Sphere(Ball.RADIUS),
+        material: Ball.CONTACT_MATERIAL
+      });
+    
+      body.linearDamping = body.angularDamping = 0.5; // Hardcode
+      body.allowSleep = true;
+    
+      // Sleep parameters
+      body.sleepSpeedLimit = 2; // Body will feel sleepy if speed< 1 (speed == norm of velocity)
+      body.sleepTimeLimit = 0.1; // Body falls asleep after 0.1s of sleepiness
+    
+      return body;
+    }
+}
+
+export { Ball };
---
+++ courses/threejs-cookbook/complete/physics/pool/Table.js
@@ -0,0 +1,24 @@
+import * as CANNON from 'cannon';
+
+class Table{
+
+    static LENGTH = 2.7432;
+    static WIDTH = 1.3716;
+    static HEIGHT = 0.06;
+    static FLOOR_MATERIAL = new CANNON.Material('floorMaterial');
+ 
+    constructor(game){
+        const shape = new CANNON.Box(new CANNON.Vec3( Table.LENGTH/2, 0.1, Table.WIDTH/2));
+        const body = new CANNON.Body({
+            mass: 0, // mass == 0 makes the body static
+            material: Table.FLOOR_MATERIAL,
+            shape
+        });
+        body.position.y = -0.1;
+        game.world.addBody(body);
+        game.helper.addVisual(body, 0x00FF00, false, true);
+        this.rigidBody = body;
+    }
+}
+
+export { Table };
